142f01b94dffb7bb465bdb79a853fa39b8f8bd56
burkemm <burkemm84@gmail.com>
2021-02-26 14:50:22 -0600

    commit tree 7e1f57cb9cf793800e38da5c38e1f114d3aaf537

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
     if array.length == 1
-        array.join
+       array.join
     elsif array.length == 2
-        array.join( " and ")
+       array.join( " and ")
     elsif array.length >= 3
-        last_element = array.pop
-        array.join(", ") << (", and #{last_element}")
+       last_element = array.pop
+       array.join(", ") << (", and #{last_element}")
     end
 end
